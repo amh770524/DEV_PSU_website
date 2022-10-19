bdc1ab30bcb5e9096092d50ea9fe0227aa1a394b
Andrew Ho <amh7705@psu.edu>
2022-10-18 20:34:09 -0400

    Intro to jQuery

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-document.getElementById('demo').style.color = 'red';
+$(document).ready(() => {
+    $("#btn1").mousedown(() => {
+        //$("p").slideToggle();
+        let p = $("p");
+        p.animate({height: "100px"}, "fast")
+    });
+});
 
-function myFunc() {
-    alert('WARNNING: ALERT');
-}
+// document.getElementById('demo').style.color = 'red';
 
-function lightMode() {
-    document.getElementById('pagestyle').setAttribute('href', "./style.css")
-}
+// function myFunc() {
+//     alert('WARNNING: ALERT');
+// }
 
-function darkMode() {
-    document.getElementById('pagestyle').setAttribute('href', "./style-dark.css");
-}
+// function lightMode() {
+//     document.getElementById('pagestyle').setAttribute('href', "./style.css");
+// }
+
+// function darkMode() {
+//     document.getElementById('pagestyle').setAttribute('href', "./style-dark.css");
+// }
+
+// function key_pressed(input) {
+//     console.log(input);
+//     document.getElementById('keypressed_text').innerHTML = input;
+// }
+
+// const key = function keyEvent(e) {
+//     key_pressed(e.key);
+//     if(e.key === "f") {
+//         alert("F key pressed");
+//     }
+// }
+
+// document.addEventListener("keypress", key);
